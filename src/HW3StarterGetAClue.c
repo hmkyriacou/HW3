0234a33cb547117eeea395161509a1cffda4bfc9
hmkyriacou <hmkyriacou14@gmail.com>
2020-02-07 19:56:15 -0500

    New Commit

--- src/HW3StarterGetAClue.c
+++ src/HW3StarterGetAClue.c
@@ -13,22 +13,23 @@
 #include "Room.h"
 #include "LinkedList.h"
 #include "structures.h"
+#include <string.h>
 
 int main(void) {
 	puts("!!!Mystery Mansion aka HW3!!!\n\n");
     
     FILE *AdjMat = fopen("AdjMatrix.txt", "w");
 	fprintf(AdjMat, "0 1 0 0 0 0 0 0 0 0 0 0\n");
-	fprintf(AdjMat, "1 0 1 0 0 0 0 0 0 0 1 0\n");
+	fprintf(AdjMat, "1 0 1 0 0 0 0 0 0 1 1 0\n");
 	fprintf(AdjMat, "0 1 0 1 0 0 1 0 0 0 1 0\n");
 	fprintf(AdjMat, "0 0 1 0 1 0 0 0 0 0 1 0\n");
 	fprintf(AdjMat, "0 0 0 1 0 1 0 0 0 1 1 0\n");
 	fprintf(AdjMat, "0 0 0 0 1 0 1 0 0 0 1 0\n");
 	fprintf(AdjMat, "0 0 1 0 0 1 0 1 0 0 0 0\n");
 	fprintf(AdjMat, "0 0 0 0 0 0 1 0 1 0 1 0\n");
-	fprintf(AdjMat, "0 1 0 0 0 0 0 1 0 1 1 0\n");
-	fprintf(AdjMat, "0 0 0 0 1 0 0 0 1 0 0 0\n");
-	fprintf(AdjMat, "0 1 1 1 1 1 0 1 1 0 1 0\n");
+	fprintf(AdjMat, "0 0 0 0 0 0 0 1 0 1 1 0\n");
+	fprintf(AdjMat, "0 1 0 0 1 0 0 0 1 0 0 0\n");
+	fprintf(AdjMat, "0 1 1 1 1 1 0 1 1 0 0 1\n");
 	fprintf(AdjMat, "0 0 0 0 0 0 0 0 0 0 1 0\n");
 
 	fclose(AdjMat);
@@ -54,37 +55,79 @@ int main(void) {
     scanf("%d", &gameMode);
     
     if (gameMode == 1){
-        puts("Thank you for doing a through examination. Which room would you like to start in?");
+        puts("Thank you for doing a through examination. Which room number would you like to start in?");
         int startingRoom;
         scanf("%d", &startingRoom);
+        
+        DLLNode* walkThroughList = makeEmptyLinkedList();
+        Room* searchedRoom = findRoom(startingRoom, roomList);
+        
+        searchedRoom->discovered = true;
+        searchedRoom->searched = true;
+        savePayload(walkThroughList, searchedRoom);
 
-        //TODO: Finish gameMode 1    
+        int* adjRooms = findAdjRooms("AdjMatrix.txt", searchedRoom->roomNum);
+
+        int DUMMY = printRoomInfo(searchedRoom);
+
+        //int numOfSearchedRooms = 1;
+        //while (numOfSearchedRooms <= 12){
+            
+            puts("\n\nBeggining bredth-first-search...\n");
+
+            printf("From the %s, you can see %d rooms.\n\n", searchedRoom->roomName, *(adjRooms-1));
+
+            puts("The adjacent rooms are...");
+            
+            printf("\n--------DEBUG--------\nfindAdjRooms size=%d\nfindAdjRooms[0]=%d\nfindAdjRooms[1]=%d\nfindAdjRooms[2]=%d\nfindAdjRooms[3]=%d\n\n",*(adjRooms-1), adjRooms[0], adjRooms[1], adjRooms[2], adjRooms[3]);
+            printf("Room name: %s\n\n", findRoom(10, roomList)->roomName);
+
+            for (int i = 0; i < *(adjRooms-1); i++){
+                printf("%c", i==2 ? '1' : '\0');
+                Room* curRoom = findRoom(adjRooms[i], roomList);
+                printf("%c", i==2 ? '2' : '\0');
+                printf("The %s\n", curRoom->roomName);
+                printf("%c", i==2 ? '3' : '\0');
+                
+                curRoom->discovered = true;
+                printf("%c", i==2 ? '4' : '\0');
+                savePayload(walkThroughList, curRoom);
+                printf("%c", i==2 ? '5' : '\0');
+
+            }
+            printf("\n");
+        //}
 
     } else {
         puts("What roomNumber would you like to see the information for?");
         int lookAtRoom;
         scanf("%d", &lookAtRoom);
         
-        printf("Looking at room %d...\n", lookAtRoom);
-
         Room* searchedRoom = findRoom(lookAtRoom, roomList);
-        printf("The room number is: %d\n", searchedRoom->roomNum);
-        printf("The room name is: %s\n", searchedRoom->roomName);
-        printf("The number of clues is: %d\n", searchedRoom->numClues);
-    
+        int DUMMY = printRoomInfo(searchedRoom); 
     }
 
     //printf("%d\n", *(adjRooms-1));
 
-    /*
-    for (int i = 0; i < *(adjRooms-1); i++){
-        
-        printf("%d, ", adjRooms[i]);
-
-    }
-    printf("\n");
-    */
 
 	return EXIT_SUCCESS;
+}
+
+int printRoomInfo(Room* r){
+    
+    int n = r->roomNum;
+    int c = r->numClues;
+    char name[50];
+    strcpy(name, r->roomName);
+    bool s = r->searched;
+    bool d = r->discovered;
+
+    printf("\nShowing information for the %s... \n", name);
+    printf("The room number is: %d\n", n);
+    printf("The number of clues is %d\n", c);
+    printf("The room %s searched\n", s ? "has been" : "has not been");
+    printf("The room %s discovered\n\n", d ? "has been" : "has not been");
+
+    return 0;
 }
 
